45614ce2504b7073f4f10bb0de3765eebfbabdd9
lessw <lessw@etrillium.com>
2023-01-25 23:15:07 +0000

    updates for model parallel size, num_heads, etc.

--- 2d.py
+++ 2d.py
@@ -3,7 +3,7 @@ import os
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
-from llm.src.mosaic_gpt import GPTBlock
+from llm.src.mosaic_gpt import GPTBlock, TorchCausalAttention
 from omegaconf import DictConfig
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 
@@ -24,3 +24,75 @@ except BaseException as e:
     pass
 
 print(f"TP is available == {TP_AVAILABLE}")
+
+
+def setup(rank, world_size):
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "12355"
+    # initialize the process group
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+    torch.cuda.set_device(rank)
+    print(f" rank {rank} has joined")
+
+
+def cleanup():
+    dist.destroy_process_group()
+
+
+def demo_2d(rank, args):
+    """
+    Main body of the demo of a basic version of 2d parallel by using
+    PyTorch native APIs.
+    """
+    setup(rank, args.world_size)
+    # 2-D mesh is [dp, tp]
+    twod_mesh = DeviceMesh(
+        device_type="cuda",
+        mesh=torch.arange(0, args.world_size).view(args.model_parallel_size, -1),
+    )
+    gpt_config = DictConfig(
+        {
+            "alibi": False,
+            "attn_impl": "torch",  # Only support torch now
+            "d_model": 1024,
+            "n_heads": 8,
+            "attn_pdrop": 0.01,
+            "mlp_ratio": 4,
+            "resid_pdrop": 0.01,
+        }
+    )
+    model = GPTBlock(gpt_config, causal_attn_cls=TorchCausalAttention).cuda(rank)
+    model = parallelize_module(
+        model, twod_mesh, {"causal_attn": PairwiseParallel(), "mlp": PairwiseParallel()}
+    )
+    fsdp_pg = twod_mesh.get_dim_groups()[0]
+    model = FSDP(model, process_group=fsdp_pg)
+    # Create a optimizer for the parallelized module.
+    LR = 0.25
+    _optimizer = torch.optim.SGD(model.parameters(), lr=LR)
+    # Perform a num of iterations of forward/backward
+    # and optimizations for the sharded module.
+    cleanup()
+
+
+def run_demo(demo_fn, args):
+    mp.spawn(demo_fn, args=(args,), nprocs=args.world_size, join=True)
+
+
+if __name__ == "__main__":
+    n_gpus = torch.cuda.device_count()
+    parser = argparse.ArgumentParser()
+    # This is passed in via cmd
+    parser.add_argument("--world_size", type=int, default=n_gpus)
+    parser.add_argument("--iter_nums", type=int, default=10)
+    parser.add_argument("--model_parallel_size", type=int, default=2)
+    args = parser.parse_args()
+    # The main entry point is called directly without using subprocess
+    if n_gpus < 2:
+        print("Requires at least 2 GPUs to run.")
+    elif not TP_AVAILABLE:
+        print(
+            "PyTorch doesn't have Tensor Parallelism available," " need nightly build."
+        )
+    else:
+        run_demo(demo_2d, args)
